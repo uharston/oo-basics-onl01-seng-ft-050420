f543f141dc86f5cfe2207b424b8f3ef3d63fbd54
Uriah Harston <uharston@gmail.com>
2020-05-12 14:19:38 +0000

    Done.

--- lib/book.rb
+++ lib/book.rb
@@ -36,8 +36,20 @@ class Book
     puts "Flipping the page...wow, you read fast!"
   end
 
+end
 
 
+class Book
+  attr_accessor :author, :pages, :genre
+  attr_reader :title
+  def initialize(title)
+    @title = title
+  end
+
+
+  def turn_page
+    puts "Flipping the page...wow, you read fast!"
+  end
 
 
 end
